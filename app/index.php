aef0dfeae27b6e333686de1e485dbe99770aa050
Akhilkp27 <akhil499@gmail.com>
2025-03-29 14:31:03 +0530

    second commit

--- app/index.php
+++ app/index.php
@@ -10,4 +10,27 @@
     test app
    </h3> 
 </body>
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDEOWVGNTe9L2mi1aBdP_xiIvAxmQa79yA",
+    authDomain: "ceetee-app-cloud.firebaseapp.com",
+    projectId: "ceetee-app-cloud",
+    storageBucket: "ceetee-app-cloud.firebasestorage.app",
+    messagingSenderId: "300192532879",
+    appId: "1:300192532879:web:9888a676b444a64abc23fa",
+    measurementId: "G-VRKZXH0LDH"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 </html>
